b7703489fc431d75a159682306c3396d9ff20174
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-04-15 20:52:21 +0530

    Changes for main section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import StatsCard from "./StatsCard";
 
 function Main() {
-  return <div>main component</div>;
+  return (
+    <div className="w-[100%] h-screen">
+      <div className="w-[100%] h-[5%] shadow"></div>
+      <div className="p-4 flex">
+        <StatsCard text="Current Balance" amount="$550" />
+        <StatsCard text="Income" amount="$1500" />
+        <StatsCard text="Expenses" amount="$1050" />
+      </div>
+    </div>
+  );
 }
 
 export default Main;
---
+++ src/components/StatsCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function StatsCard({ text, amount }) {
+  return (
+    <div className="shadow w-[25%] p-4 rounded mr-4">
+      <p className="text-sm mb-1">{text}</p>
+      <p className="text-2xl">{amount}</p>
+    </div>
+  );
+}
+
+export default StatsCard;
