5fa6a06f82395da126e29ce8616d6f11e89a5b46
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-04-18 17:19:49 +0530

    Minor changes in expense modal

--- src/components/ExpenseModal.jsx
+++ src/components/ExpenseModal.jsx
@@ -32,31 +32,40 @@ export default function ExpenseModal({ isOpen, onClose, onSubmit }) {
       <div className="bg-white rounded-2xl shadow-lg p-6 w-96">
         <h2 className="text-xl font-semibold mb-4">Add New Expense</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
+          <label htmlFor="amountType" className="text-xs">
+            Amount Type
+          </label>
           <select
             name="amountType"
             value={formData.amountType}
             onChange={handleChange}
-            className="w-full border rounded p-2 cursor-pointer"
+            className="w-full border rounded px-2 py-1 cursor-pointer"
             required
           >
             <option value="">Select Amount Type</option>
             <option value="Income">Income</option>
             <option value="Expense">Expense</option>
           </select>
+          <label htmlFor="amount" className="text-xs">
+            Amount
+          </label>
           <input
             type="number"
             name="amount"
             placeholder="Amount"
             value={formData.amount}
             onChange={handleChange}
-            className="w-full border rounded p-2"
+            className="w-full border rounded px-2 py-1"
             required
           />
+          <label htmlFor="spendType" className="text-xs">
+            Spend Type
+          </label>
           <select
             name="spendType"
             value={formData.spendType}
             onChange={handleChange}
-            className="w-full border rounded p-2 cursor-pointer"
+            className="w-full border rounded px-2 py-1 cursor-pointer"
             required
           >
             <option value="">Select Type</option>
@@ -66,33 +75,39 @@ export default function ExpenseModal({ isOpen, onClose, onSubmit }) {
             <option value="Shopping">Shopping</option>
             <option value="Other">Other</option>
           </select>
+          <label htmlFor="date" className="text-xs">
+            Date
+          </label>
           <input
             type="date"
             name="date"
             value={formData.date}
             onChange={handleChange}
-            className="w-full border rounded p-2"
+            className="w-full border rounded px-2 py-1"
             required
           />
+          <label htmlFor="remarks" className="text-xs">
+            Remarks
+          </label>
           <input
             type="text"
             name="remarks"
             placeholder="Remarks"
             value={formData.remarks}
             onChange={handleChange}
-            className="w-full border rounded p-2"
+            className="w-full border rounded px-2 py-1"
           />
           <div className="flex justify-end gap-2">
             <button
               type="button"
-              className="text-gray-600 cursor-pointer"
+              className="text-gray-600 px-4 py-1 rounded cursor-pointer"
               onClick={onClose}
             >
               Cancel
             </button>
             <button
               type="submit"
-              className="bg-green-600 text-white px-4 py-2 rounded cursor-pointer"
+              className="bg-black text-white px-4 py-1 rounded cursor-pointer"
             >
               Save
             </button>
