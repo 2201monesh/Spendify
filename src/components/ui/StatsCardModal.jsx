bdfa4154419ed700633887075be8c21d6a3b9d35
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-05-13 20:10:43 +0530

    Added styling for stats card modal

--- src/components/ui/StatsCardModal.jsx
+++ src/components/ui/StatsCardModal.jsx
@@ -9,18 +9,24 @@ function StatsCardModal() {
   const closeModal = () => setStatsCardModal({ flag: false, type: "" });
 
   return (
-    <div className="fixed inset-0 bg-black/50 backdrop-blur-[2px] flex justify-center items-center z-50 w-[50px] h-[50px]">
-      <button onClick={closeModal}>Close</button>
+    <div className="fixed inset-0 bg-black/50 backdrop-blur-[2px] flex justify-center items-center z-50">
+      {/* Modal Box */}
+      <div className="bg-white w-[400px] h-[450px] rounded-lg shadow-lg p-6 relative">
+        <button
+          onClick={closeModal}
+          className="absolute top-2 right-2 text-gray-500 hover:text-black cursor-pointer"
+        >
+          Close
+        </button>
 
-      {statsCardModal.type === "Income" ? (
-        <p>Showing income details...</p>
-      ) : (
-        <></>
-      )}
-      {statsCardModal.type === "Expenses" && <p>Showing expenses details...</p>}
-      {statsCardModal.type === "Current Balance" && (
-        <p>Showing savings details...</p>
-      )}
+        {statsCardModal.type === "Income" && <p>Showing income details...</p>}
+        {statsCardModal.type === "Expenses" && (
+          <p>Showing expenses details...</p>
+        )}
+        {statsCardModal.type === "Current Balance" && (
+          <p>Showing savings details...</p>
+        )}
+      </div>
     </div>
   );
 }
